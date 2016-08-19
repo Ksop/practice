ba431b762b5a8bd974bc7cefe91ac8674c0839cb
Kuba Sop <cuba.web.kg@gmail.com>
2016-08-19 23:47:34 +0600

    Main page with book search form

--- js/script.js
+++ js/script.js
@@ -4,7 +4,12 @@
   var toggleNav = document.querySelector(".page-header__open-nav");
   var closeNav = document.querySelector(".page-header__close-nav");
 
-  toggleNav.addEventListener("click", function() {
+  var toggleSearchForm = document.querySelector(".book__search-link");
+  var bookSearchForm = document.querySelector(".book__search");
+
+  toggleNav.addEventListener("click", function(e) {
+
+    e.preventDefault();
 
     if ( mainNav.classList.contains("main-nav--closed") ) {
       mainNav.classList.remove("main-nav--closed");
@@ -16,12 +21,24 @@
 
   });
 
-  closeNav.addEventListener("click", function() {
+  closeNav.addEventListener("click", function(e) {
+
+    e.preventDefault();
 
     if ( mainNav.classList.contains("main-nav--opened") ) {
       mainNav.classList.remove("main-nav--opened");
       mainNav.classList.add("main-nav--closed");
     }
+
+  });
+
+  toggleSearchForm.addEventListener("click", function(e) {
+
+    e.preventDefault();
+
+    // if ( bookSearchForm.classList.contains("book__search--closed") ) {
+      bookSearchForm.classList.toggle("book__search--closed");
+    // }
 
   });
 
